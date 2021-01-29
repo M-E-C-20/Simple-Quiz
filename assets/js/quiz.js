2db3dea8f85d3b699050a84935bff79db6162248
M-E-C-20 <mcasey8511@gmail.com>
2021-01-29 01:32:43 +0000

    add slide function between questions, add styling

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -15,17 +15,19 @@ function buildQuiz() {
                 // ...add an HTML radio button
                 answers.push(
                     `<label>
-            <input type="radio" name="question${questionNumber}" value="${letter}">
-            ${letter} :
-            ${currentQuestion.answers[letter]}
-          </label>`
+                        <input type="radio" name="question${questionNumber}" value="${letter}">
+                        ${letter} :
+                        ${currentQuestion.answers[letter]}
+                    </label>`
                 );
             }
 
             // add this question and its answers to the output
             output.push(
-                `<div class="question"> ${currentQuestion.question} </div>
-        <div class="answers"> ${answers.join('')} </div>`
+                `<div class="slide">
+                    <div class="question"> ${currentQuestion.question} </div>
+                    <div class="answers"> ${answers.join("")} </div>
+                 </div>`
             );
         }
     );
@@ -68,17 +70,45 @@ function showResults() {
     resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
 }
 
+function showSlide(n) {
+    slides[currentSlide].classList.remove('active-slide');
+    slides[n].classList.add('active-slide');
+    currentSlide = n;
+    if (currentSlide === 0) {
+        previousButton.style.display = 'none';
+    }
+    else {
+        previousButton.style.display = 'inline-block';
+    }
+    if (currentSlide === slides.length - 1) {
+        nextButton.style.display = 'none';
+        submitButton.style.display = 'inline-block';
+    }
+    else {
+        nextButton.style.display = 'inline-block';
+        submitButton.style.display = 'none';
+    }
+}
+
+function showNextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function showPreviousSlide() {
+    showSlide(currentSlide - 1);
+}
+
 // variables
 const quizContainer = document.getElementById('quiz');
 const resultsContainer = document.getElementById('results');
 const submitButton = document.getElementById('submit');
 const myQuestions = [
     {
-        question: "Who invented JavaScript?",
+        question: "Who shot JR?",
         answers: {
             a: "Douglas Crockford",
             b: "Sheryl Sandberg",
-            c: "Brendan Eich"
+            c: "My Anus"
         },
         correctAnswer: "c"
     },
@@ -106,5 +136,16 @@ const myQuestions = [
 // display quiz right away
 buildQuiz();
 
+// Pagination
+const previousButton = document.getElementById("previous");
+const nextButton = document.getElementById("next");
+const slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+
+// Show the first slide
+showSlide(currentSlide);
+
 // on submit, show results
 submitButton.addEventListener('click', showResults);
+previousButton.addEventListener("click", showPreviousSlide);
+nextButton.addEventListener("click", showNextSlide);
